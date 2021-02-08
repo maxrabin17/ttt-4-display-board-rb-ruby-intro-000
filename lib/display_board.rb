ffe9e664b3e38778f0f442938b1018471f7aaf2d
Max Rabin <max.rabin17@gmail.com>
2021-02-08 18:52:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,4 +8,5 @@ def display_board(board)
   puts "  #{board[6]} |  #{board[7]} |  #{board[8]} "
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["x", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
